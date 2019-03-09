39100ff5fff2a54b5e9bcbff9c0713d719a0f4b4
ndri <andri@tfwno.gf>
2019-03-09 10:52:37 +0200

    They changed the date format

--- fbeventscrape.py
+++ fbeventscrape.py
@@ -83,7 +83,7 @@ def parse_fb_date(date: str) -> datetime.datetime:
 
     elif re.match(regex2, date):
         groups = re.match(regex2, date).groups()
-        month = int(groups[1])
+        #month = int(groups[1])
         #month = short_months.index(groups[1].lower()) + 1
         month = months.index(groups[1].lower()) + 1
 
