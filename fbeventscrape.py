ad2b55905da20c65e56add0dd65bffb2cbfbff0d
ndri <andri@tfwno.gf>
2019-03-25 12:11:42 +0200

    Fix German short months

--- fbeventscrape.py
+++ fbeventscrape.py
@@ -63,6 +63,9 @@ def parse_fb_date(date: str) -> datetime.datetime:
         "januar", "februar", "märz", "april", "mai", "juni",
         "juli", "august", "september", "oktober", "november", "dezember"
     ]
+    short_months = [
+        "jan.", "feb.", "märz.", "apr.", "mai", "juni",
+        "juli", "aug.", "sep.", "okt.", "nov.", "dez."
 
     # Estonian months
     """
@@ -86,7 +89,7 @@ def parse_fb_date(date: str) -> datetime.datetime:
         groups = re.match(regex2, date).groups()
         #month = int(groups[1])
         #month = short_months.index(groups[1].lower()) + 1
-        month = months.index(groups[1].lower()) + 1
+        month = short_months.index(groups[1].lower()) + 1
         year = datetime.datetime.now().year;
 
     else:
