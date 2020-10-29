3d83485551e29aa8c47c69a93e0e71fabde5dc8d
EduardNot <eduard.rudi@inbox.ru>
2020-10-29 17:06:35 +0200

    Removed testing stuff

--- website/cypress/integration/test.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Test index_cta_text', () => {
-    it('Visit CMS and edit', () => {
-        cy.visit('http://localhost:8080/admin/cms/field/5f9878ee24dd5f00442d36be')
-        cy.get('textarea').should('contain', 'asdsa!')
-    })
-})
