f1e8a9c05a98545cd5c5ad3e4fbe5771a2d304f2
EduardNot <eduard.rudi@inbox.ru>
2020-10-29 16:44:45 +0200

    Added test

--- website/cypress/integration/index_cta_text.spec.js
+++ website/cypress/integration/index_cta_text.spec.js
@@ -1,5 +1,5 @@
 describe('Test index_cta_text', () => {
-    it('Visit CMS and edit', () => {
+    it('Visit CMS and edit text', () => {
         cy.visit('http://localhost:8080/admin/cms/')
         cy.contains('index_cta_text').click()
         cy.get('textarea').invoke('val').then((text) => {
@@ -13,7 +13,28 @@ describe('Test index_cta_text', () => {
             cy.get('button').click()
             cy.visit('http://localhost:8080/')
             cy.get('#joininfo').should('contain', 'Hello!')
-            cy.visit('http://localhost:8080/admin/cms/field/5f9878ee24dd5f00442d36be')
+            cy.visit('http://localhost:8080/admin/cms/')
+            cy.contains('index_cta_text').click()
+            cy.get('textarea').clear().type(text)
+            cy.get('button').click()
+        })
+    })
+
+    it('Change HTML tag', () => {
+        cy.contains('index_cta_text').click()
+        cy.get('textarea').invoke('val').then((text) => {
+            cy.get('textarea').clear().type(`
+            <h2>Hei!</h2>
+            <h2>Liitumine MITSi on avatud!</h2>
+            <a class="btn gradient"
+               href="/liitumine"
+               target="_blank" rel="noopener">Liitu</a>
+               `)
+            cy.get('button').click()
+            cy.visit('http://localhost:8080/')
+            cy.contains('h2', 'Hei!')
+            cy.visit('http://localhost:8080/admin/cms/')
+            cy.contains('index_cta_text').click()
             cy.get('textarea').clear().type(text)
             cy.get('button').click()
         })
