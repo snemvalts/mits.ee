0d88ce8ab7d66c0cd03fb3f076ab5cd7a09d8f69
Sander Nemvalts <snemvalts@gmail.com>
2020-11-07 14:38:17 +0200

    re-enable CSP

--- website/src/app.js
+++ website/src/app.js
@@ -82,11 +82,7 @@ app.use((req, res, next) => {
 });
 
 app.use(compression());
-app.use(helmet(
-  {
-    contentSecurityPolicy: false,
-  },
-));
+app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
