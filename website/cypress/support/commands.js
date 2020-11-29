6adb947dc16df2cc74663268ae93ccf3d78c6d79
EduardNot <eduard.rudi@inbox.ru>
2020-11-29 19:25:45 +0200

    Added test for file upload

--- website/cypress/support/commands.js
+++ website/cypress/support/commands.js
@@ -23,6 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'cypress-file-upload';
 
 Cypress.Commands.add('changeValue', (container, changedText) => {
     const selectAllKeyCombo = Cypress.platform === 'darwin' ? '{cmd}a' : '{ctrl}a';
@@ -43,4 +45,18 @@ Cypress.Commands.add('resetField', (container) => {
     cy.fixture('../../src/models/cms-default-values.json').then((json) => {
         cy.changeValue(container, json[container])
     })
+})
+
+Cypress.Commands.add('uploadFile', (path, fileLocation) => {
+    cy.visit('http://localhost:8080/admin/cms/');
+    cy.wait(1000);
+    cy.get('#folder').select(path);
+    cy.get('input[type=file]').attachFile(fileLocation);
+    cy.get('input[type=submit]').click();
+    cy.readFile(`src/public/${path}/test-file.png`).should('exist');
+})
+
+Cypress.Commands.add('deleteFile', (path) => {
+    cy.exec(`rm src/public/${path}/test-file.png`);
+    cy.readFile(path).should('not.exist');
 })
